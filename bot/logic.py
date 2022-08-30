742eca89d99647b3eea2995487cd140b95254f33
hugotms <hugotms@icloud.com>
2022-08-30 02:05:29 +0200

    fix: using datetime.utcnow for report instead of datetime.now

--- bot/logic.py
+++ bot/logic.py
@@ -33,7 +33,7 @@ def start(exchange_client, crypto, account):
     return "Placed action on " + crypto.instrument_code + " (OWNED: " + str(round(crypto.owned, 4)) + ").\n"
 
 def report(database, watching_currencies, ignore_currencies, taxe_rate):
-    response = database.getPastPerformance(datetime.datetime.now() - timedelta(days=1), watching_currencies, ignore_currencies)
+    response = database.getPastPerformance(datetime.datetime.utcnow() - timedelta(days=1), watching_currencies, ignore_currencies)
     if response is None:
         return ""
     response = json.loads(response)
@@ -54,7 +54,7 @@ def report(database, watching_currencies, ignore_currencies, taxe_rate):
     
     today = datetime.datetime.now()
     if today.weekday() == 0:
-        response = database.getPastPerformance(datetime.datetime.now() - timedelta(weeks=1), watching_currencies, ignore_currencies)
+        response = database.getPastPerformance(datetime.datetime.utcnow() - timedelta(weeks=1), watching_currencies, ignore_currencies)
         if response is None:
             return message
         response = json.loads(response)
@@ -74,7 +74,7 @@ def report(database, watching_currencies, ignore_currencies, taxe_rate):
                 "€\n"
     
     if today.month != (today + timedelta(days=1)).month:
-        response = database.getPastPerformance(datetime.datetime.now() - relativedelta(months=1), watching_currencies, ignore_currencies)
+        response = database.getPastPerformance(datetime.datetime.utcnow() - relativedelta(months=1), watching_currencies, ignore_currencies)
         if response is None:
             return message
         response = json.loads(response)
