e58087a0867e0099179ac0388da0e5def7af4b15
Hugo TOMASI <45932591+hugotms@users.noreply.github.com>
2023-01-13 11:35:18 +0100

    fix: reporting were missing fees (#29)
    * fix: finxing reporting by adding fees

--- server/db.py
+++ server/db.py
@@ -171,7 +171,7 @@ class Mongo:
         
         return self.find("actives", query)
     
-    def getPastPerformance(self, report, watching_currencies, ignore_currencies, instrument_code=None):
+    def getPastPerformance(self, report, parameters, instrument_code=None):
         if self.client is None:
             return None
 
@@ -186,20 +186,20 @@ class Mongo:
             }
         }
 
-        if len(ignore_currencies) != 0:
+        if len(parameters.ignore_currencies) != 0:
             query["instrument_code"] = {
-                "$nin": ignore_currencies
+                "$nin": parameters.ignore_currencies
             }
             query2["_id"] = {
-                "$nin": ignore_currencies
+                "$nin": parameters.ignore_currencies
             }
         
-        elif len(watching_currencies) != 0:
+        elif len(parameters.watching_currencies) != 0:
             query["instrument_code"] = {
-                "$in": watching_currencies
+                "$in": parameters.watching_currencies
             }
             query2["_id"] = {
-                "$in": watching_currencies
+                "$in": parameters.watching_currencies
             }
         
         if instrument_code is not None:
@@ -213,12 +213,15 @@ class Mongo:
             placed = float(item["placed"])
             current = float(item["current"])
 
+            report.volume += current
+
+            current *= parameters.account.takerFee
+
             if current > placed:
                 report.gain += current - placed
-            else:
-                report.loss += placed - current
+                continue
 
-            report.volume += current
+            report.loss += placed - current
         
         for item in res2:
             report.volume += float(item["placed"])
