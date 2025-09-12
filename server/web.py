2b5ca60b1c913cd40dc04ff07a273c8aa7f2bcc7
Hugo TOMASI <45932591+hugotms@users.noreply.github.com>
2025-09-13 01:05:18 +0200

    Revert API changes and remove new files

--- server/web.py
+++ server/web.py
@@ -24,12 +24,15 @@ class Api:
                 response = requests.delete(self.url, headers=self.headers, json=self.data)
             else:
                 print('Unsupported method')
-        except:
+        except Exception:
             response = None
             print('Unable to access URL')
 
         if response is not None:
             self.statusCode = response.status_code
-            self.res = response.json()
+            try:
+                self.res = response.json()
+            except ValueError:
+                print('Unable to process returned data')
 
         return self.statusCode, self.res
